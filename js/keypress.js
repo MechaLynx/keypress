1932f2dd35e0ba3bf5919a69d4e8081e221a8106
MechaLynx <mechalynx88@gmail.com>
2017-09-03 19:43:23 +0300

    Split into separate files

---
+++ js/keypress.js
@@ -0,0 +1,25 @@
+var display = document.getElementById('display');
+
+var altKey = document.getElementById('altKey');
+var ctrlKey = document.getElementById('ctrlKey');
+var shiftKey = document.getElementById('shiftKey');
+var metaKey = document.getElementById('metaKey');
+
+var code = document.getElementById('code');
+var charCode = document.getElementById('charCode');
+var key = document.getElementById('key');
+var keyCode = document.getElementById('keyCode');
+var type = document.getElementById('type');
+
+document.addEventListener('keypress', (e) => {
+  display.textContent = e.key;
+  altKey.textContent = e.altKey;
+  ctrlKey.textContent = e.ctrlKey;
+  shiftKey.textContent = e.shiftKey;
+  metaKey.textContent = e.metaKey;
+  code.textContent = e.code;
+  charCode.textContent = e.charCode;
+  key.textContent = e.key;
+  keyCode.textContent = e.keyCode;
+  type.textContent = e.type;
+});
